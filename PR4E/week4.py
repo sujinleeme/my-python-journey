dc31e4538fd0154def1aabeb80264c8ae3cd78f2
Sujin Lee <sujinlee.me@gmail.com>
2015-11-19 17:12:03 +0900

    Update week4.py

--- PR4E/week4.py
+++ PR4E/week4.py
@@ -3,7 +3,10 @@
 Course: Using Python to Access Web Data
 Topic: BeautifulSoup
 Date: 2015.11.19
-About: write a Python program that expands on http://www.pythonlearn.com/code/urllinks.py. The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags, scan for a tag that is in a particular position relative to the first name in the list, follow that link and repeat the process a number of times and report the last name you find.
+About: write a Python program that expands on http://www.pythonlearn.com/code/urllinks.py.
+The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags,
+scan for a tag that is in a particular position relative to the first name in the list,
+follow that link and repeat the process a number of times and report the last name you find.
 '''
 
 import urllib.request
