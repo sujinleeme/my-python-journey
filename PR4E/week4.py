5aab95f6da3d4505c87ac271c6334b65f2f2d195
sujinleeme <sujinlee.me@gmail.com>
2015-11-21 01:00:31 +0900

    complete week assignment

--- PR4E/week4.py
+++ PR4E/week4.py
@@ -3,19 +3,29 @@
 Course: Using Python to Access Web Data
 Topic: BeautifulSoup
 Date: 2015.11.19
+
 About: write a Python program that expands on http://www.pythonlearn.com/code/urllinks.py.
-The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags,
+The program will use urllib to read the HTML from the data files below,
+extract the href= vaues from the anchor tags,
 scan for a tag that is in a particular position relative to the first name in the list,
 follow that link and repeat the process a number of times and report the last name you find.
-'''
 
+Test Case:
+- start at http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/known_by_Fikret.html
+- Find the link at position 3 (the first name is 1). Follow that link.
+- Repeat this process 4 times. The answer is the last name that you retrieve.
+- Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
+- Last name in sequence: Anayah
+'''
+import re
 import urllib.request
 from bs4 import BeautifulSoup
 
 #input
-position = int(input('position: '))-1
-process = int(input('process: '))
-url = str(input('url: '))
+url = str(input('Enter url: '))
+position = (int(input('Enter position: '))-1)
+process = int(input('Enter process: '))
+
 
 data = []
 while process > 0:
@@ -27,4 +37,5 @@ while process > 0:
     process -= 1
 
 #last output
-print (data[-1])
+lastUrl = data[-1]
+print ((re.search(r'(?<=known_by_)[\w\.]+(?=.html)', lastUrl).group()))
