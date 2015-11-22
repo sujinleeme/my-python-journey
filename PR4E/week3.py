bdc276a9cd165b40b8eed81fdf842eb62df8c06a
Sujin Lee <sujinlee.me@gmail.com>
2015-11-22 16:01:13 +0900

    Create week3.py

---
+++ PR4E/week3.py
@@ -0,0 +1,23 @@
+#Description
+'''
+Course: Using Python to Access Web Data
+Topic: BeautifulSoup
+Date: 2015.11.12.
+About: Networked Programs
+
+#1. socket
+import socket
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('www.py4inf.com', 80))
+mysock.send(b'GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+while True:
+    data = mysock.recv(512)
+    if len(data)==0:
+        break
+    print(data.decode('utf-8'))
+mysock.close()
+
+#2. urllib
+import urllib.request file = urllib.request.urlopen('http://www.py4inf.com/code/romeo.txt')
+for line in file:
+  print(line.decode('utf-8').strip())
