a51be4e95db1d1e05df6a578e175b1b46972bf19
Sujin Lee <sujinlee.me@gmail.com>
2015-11-29 22:15:33 +0900

    Update yahoo-weatherAPI.py

--- PR4E/yahoo-weatherAPI.py
+++ PR4E/yahoo-weatherAPI.py
@@ -18,7 +18,7 @@ def celsius(fahrenheit):
 city = input("Enter your city: ")
 baseurl = 'https://query.yahooapis.com/v1/public/yql?q='
 yql_query = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="%s")'% city
-yql_url = baseurl + urllib.parse.quote(yql_query, encoding='utf-8')+ '&format=json&env=store://datatables.org/alltableswithkeys'
+yql_url = baseurl + urllib.parse.quote(yql_query, encoding='utf-8') + '&format=json&env=store://datatables.org/alltableswithkeys'
 result = urllib.request.urlopen(yql_url, context=scontext).read()
 
 #data parsing
